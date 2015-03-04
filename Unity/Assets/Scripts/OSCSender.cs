9600d5dc539247af6af499909181dccef44157b7
jlocas <jeremie.locas@hotmail.com>
2015-03-04 16:01:15 -0500

    merged python and unity folders, set up osc connection

---
+++ Unity/Assets/Scripts/OSCSender.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OSCSender : MonoBehaviour {
+
+	public void MouseSqueak(){
+		OSCHandler.Instance.SendMessageToClient("Pyo", "/mj/mouse/squeak", 1);
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+		OSCHandler.Instance.Init(); //init OSC
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//OSCHandler.Instance.SendMessageToClient("Pyo", "/test/test", "allo pyo");
+
+
+	
+	}
+}
